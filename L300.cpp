7178e68afa5eee304ac2425d79831063984911b5
Plamen Hristov <l4k3rd@gmail.com>
2018-01-07 22:20:29 +0200

    Solve: 300. Longest Increasing Subsequence

---
+++ L300.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        multiset<int> s;
+        for (auto num : nums) {
+            s.insert(num);
+            auto it = s.find(num);
+            ++it;
+            if (it != s.end()) {
+                s.erase(it);
+            }
+        }
+        return s.size();
+    }
+};
